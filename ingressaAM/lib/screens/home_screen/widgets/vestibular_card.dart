3a7e331c5851b18bd048e00e4d43b06269e16701
Rennan Alves <rennandev7@gmail.com>
2024-12-05 23:42:31 -0400

    feat: opção de deletar vestibular

--- ingressaAM/lib/screens/home_screen/widgets/vestibular_card.dart
+++ ingressaAM/lib/screens/home_screen/widgets/vestibular_card.dart
@@ -5,6 +5,9 @@ import 'package:flutter_webapi_first_course/models/vestibular.dart';
 import 'package:flutter_webapi_first_course/screens/commom/exception_dialog.dart';
 import 'package:flutter_webapi_first_course/services/vestibular_service.dart';
 
+import '../../../helpers/logout.dart';
+import '../../commom/confirmation_dialog.dart';
+
 class VestibularCard extends StatelessWidget {
   final Vestibular vestibular;
   final Function refreshFunction;
@@ -48,9 +51,40 @@ class VestibularCard extends StatelessWidget {
             maxLines: 2,
             overflow: TextOverflow.ellipsis,
           ),
-          trailing: const Icon(Icons.arrow_forward),
+          trailing: Container(child: IconButton(onPressed: (){
+            removeVestibular(context);
+          }, icon: const Icon(Icons.delete)),),
         ),
       ),
     );
+  }
+
+
+  removeVestibular(BuildContext context) {
+    VestibularService vestibularService = VestibularService();
+
+    if(vestibular!=null) {
+      showConfirmationDialog(context).then((value) {
+        if(value!=null) {
+          if(value) {
+            vestibularService.delete(vestibular!.id, token).then((value) {
+              if(value) {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Vestibular apagado com sucesso")));
+                refreshFunction();
+              }
+            }).catchError((error){
+              logout(context);
+            }, test: (error) => error is TokenNotValidException,
+
+            ).catchError((error) {
+              var innerError = error as HttpException;
+              showExceptionDialog(context, content: innerError.message);
+            }, test: (error) => error is HttpException);
+          }else {
+
+          }
+        }
+      });
+    }
   }
 }
